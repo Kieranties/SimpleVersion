2ec70a59de8e3e7cd0eca4dad5a68abf95fe1669
Kieranties <Kieran@Kieranties.com>
2018-11-14 22:25:10 +0000

    Tweaking build

--- build.ps1
+++ build.ps1
@@ -3,12 +3,9 @@ param(
 	[String]$ArtifactsPath = (New-Item "$PSScriptRoot\artifacts" -ItemType Container -Force | ForEach-Object FullName)
 )
 
+
 # Clean artifacts
 Get-ChildItem $ArtifactsPath | Remove-Item -Recurse
 
 # Publish self-contained app
 dotnet publish "$SrcPath\SimpleVersion.Command\SimpleVersion.Command.csproj" --output $ArtifactsPath
-
-# Package
-$destination = "$ArtifactsPath\SimpleVersion.$Version$fullLabel.zip"
-Compress-Archive -Path $ArtifactsPath\*.exe,$PSScriptroot\Readme.md -DestinationPath $destination
