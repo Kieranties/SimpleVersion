4bb4aa65b6f6090a512c4bf31b96364b3892e649
Kieranties <Kieran@Kieranties.com>
2019-02-05 20:34:35 +0000

    #8 - Implement integration tests for command project and existing repo

--- integration/SimpleVersion.Command.Tests.ps1
+++ integration/SimpleVersion.Command.Tests.ps1
@@ -16,25 +16,43 @@ function Invoke{
     }
 }
 
+function Validate {
+	param(
+		[PSCustomObject]$Result,
+		[ScriptBlock]$AsError = $null,
+		[ScriptBlock]$AsSuccess = $null
+	)
+
+	if($AsError) {
+		$Result.ExitCode | Should -Be -1
+		$errorMessage =  $Result.Output
+		& $AsError $errorMessage
+	} else {
+		$Result.ExitCode | Should -Be 0
+		$json = ConvertFrom-Json ($Result.Output -join '')
+		& $AsSuccess $json
+	}
+}
+
 Describe 'SimpleVersion.Command'{
 	Context 'Working Directory' {
 		Context 'Not a git repo' {
-			BeforeAll { Push-Location $env:TEMP }
+			BeforeAll { Push-Location $Testdrive }
 			AfterAll { Pop-Location }
 
 			It 'Throws error if repository cannot be found'{
 
-				$expectedError = "[Error] Could not find git repository at '$pwd' or any parent directory"
 				$result = Invoke
 
-				$result.Output | Should -Be $expectedError
-				$result.ExitCode | Should -Be -1
+				Validate $result -AsError {
+					$errorMEssage | Should -Be "[Error] Could not find git repository at '$pwd' or any parent directory"
+				}
 			}
 		}
 
 		Context 'Existing Git Repo'{
 			BeforeAll {
-				$dir = New-Item "${env:TEMP}\$(Get-Random)" -ItemType Directory
+				$dir = New-Item "${TestDrive}\$(Get-Random)" -ItemType Directory
 				Push-Location $dir
 				git init
 			}
@@ -46,11 +64,90 @@ Describe 'SimpleVersion.Command'{
 
 			It 'Throws error if no commits for version file'{
 
-				$expectedError = "[Error] Could not find git repository at '{$($dir.fullname)}' or any parent directory"
 				$result = Invoke
 
-				$result.Output | Should -Be $expectedError
-				$result.ExitCode | Should -Be -1
+				Validate $result -AsError {
+					$errorMEssage | Should -Be "[Error] No commits found for '.simpleversion.json'"
+				}
+			}
+
+			It 'Returns base values for initial commit'{
+				Copy-Item $PSScriptRoot\assets\.simpleversion.json -Destination $pwd
+				git add *
+				git commit -m 'Initial commit'
+
+				$expectedSha = git rev-parse HEAD
+				$result = Invoke
+
+				Validate $result -AsSuccess {
+					$json.Version | Should -Be '0.1.0'
+					$json.Major | Should -Be 0
+					$json.Minor | Should -Be 1
+					$json.Patch | Should -Be 0
+					$json.Revision | Should -Be 0
+					$json.Height | Should -Be 1
+					$json.HeightPadded | Should -Be '0001'
+					$json.Sha | Should -Be $expectedSha
+					$json.BranchName | Should -Be 'master'
+					$json.Formats.Semver1 | Should -Be '0.1.0-alpha1-0001'
+					$json.Formats.Semver2 | Should -Be '0.1.0-alpha1.1'
+				}
+			}
+
+			It 'Increments for each commit' {
+				Copy-Item $PSScriptRoot\assets\.simpleversion.json -Destination $pwd
+				git add *
+				git commit -m 'Initial commit'
+				git commit -m 'empty' --allow-empty
+				git commit -m 'empty' --allow-empty
+				git commit -m 'empty' --allow-empty
+
+				$expectedSha = git rev-parse HEAD
+				$result = Invoke
+				Validate $result -AsSuccess {
+					$json.Version | Should -Be '0.1.0'
+					$json.Major | Should -Be 0
+					$json.Minor | Should -Be 1
+					$json.Patch | Should -Be 0
+					$json.Revision | Should -Be 0
+					$json.Height | Should -Be 4
+					$json.HeightPadded | Should -Be '0004'
+					$json.Sha | Should -Be $expectedSha
+					$json.BranchName | Should -Be 'master'
+					$json.Formats.Semver1 | Should -Be '0.1.0-alpha1-0004'
+					$json.Formats.Semver2 | Should -Be '0.1.0-alpha1.4'
+				}
+			}
+
+			It 'Resets on version change' {
+				Copy-Item $PSScriptRoot\assets\.simpleversion.json -Destination $pwd
+				git add *
+				git commit -m 'Initial commit'
+				git commit -m 'empty' --allow-empty
+				git commit -m 'empty' --allow-empty
+				$json = Get-Content $pwd\.simpleversion.json -Raw | ConvertFrom-Json
+				$json.Version = "1.0.0"
+				Set-Content $pwd\.simpleversion.json (ConvertTo-Json $json)
+				git add *
+				git commit -m 'Updated version'
+				git commit -m 'empty' --allow-empty
+
+				$expectedSha = git rev-parse HEAD
+				$result = Invoke
+
+				Validate $result -AsSuccess {
+					$json.Version | Should -Be '1.0.0'
+					$json.Major | Should -Be 1
+					$json.Minor | Should -Be 0
+					$json.Patch | Should -Be 0
+					$json.Revision | Should -Be 0
+					$json.Height | Should -Be 2
+					$json.HeightPadded | Should -Be '0002'
+					$json.Sha | Should -Be $expectedSha
+					$json.BranchName | Should -Be 'master'
+					$json.Formats.Semver1 | Should -Be '1.0.0-alpha1-0002'
+					$json.Formats.Semver2 | Should -Be '1.0.0-alpha1.2'
+				}
 			}
 		}
 	}
