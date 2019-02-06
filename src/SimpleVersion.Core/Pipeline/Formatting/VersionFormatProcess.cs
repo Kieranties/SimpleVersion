072c6609065e4a27329fb2d203af5642066d50cd
Kieranties <Kieran@Kieranties.com>
2019-02-06 22:37:38 +0000

    Fixes #44 Revision only when required

--- src/SimpleVersion.Core/Pipeline/Formatting/VersionFormatProcess.cs
+++ src/SimpleVersion.Core/Pipeline/Formatting/VersionFormatProcess.cs
@@ -12,11 +12,16 @@ namespace SimpleVersion.Pipeline.Formatting
 
             if (Version.TryParse(versionString, out var version))
             {
-                context.Result.Version = version.ToString();
-                context.Result.Major = version.Major;
-                context.Result.Minor = version.Minor;
+                context.Result.Major = version.Major > -1 ? version.Major : 0;
+                context.Result.Minor = version.Minor > -1 ? version.Minor : 0;
                 context.Result.Patch = version.Build > -1 ? version.Build : 0;
-                context.Result.Revision = version.Revision > -1 ? version.Revision : 0;
+
+                context.Result.Version = $"{context.Result.Major}.{context.Result.Minor}.{context.Result.Patch}";
+                if(version.Revision > -1)
+                {
+                    context.Result.Version += $".{version.Revision}";
+                    context.Result.Revision = version.Revision;
+                }
             }
             else
             {
