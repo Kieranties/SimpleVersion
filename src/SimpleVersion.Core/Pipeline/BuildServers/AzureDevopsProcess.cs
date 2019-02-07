2c2b48e5c9ac85b1577b42bc1cda8655f27785ef
Kieranties <Kieran@Kieranties.com>
2019-02-07 16:58:08 +0000

    WIP - Beginnings of build server fixes

---
+++ src/SimpleVersion.Core/Pipeline/BuildServers/AzureDevopsProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVersion.Pipeline.BuildServers
+{
+    public class AzureDevopsProcess : ICalculatorProcess
+    {
+        public void Apply(VersionContext context)
+        {
+            
+        }
+    }
+}
---
+++ src/SimpleVersion.Core/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleVersion
+{
+    public static class StringExtensions
+    {
+        public static bool ToBool(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+
+            if(int.TryParse(value, out var intValue))
+            {
+                return intValue == 0;
+            }
+
+            return bool.TrueString.Equals(value, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
