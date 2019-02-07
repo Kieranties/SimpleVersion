1eebe5310db8d7de3dfb9a3f5d3a8d40a23e987b
Kieranties <Kieran@Kieranties.com>
2019-02-07 19:09:36 +0000

    Add build server process for Azure Devops
    Contributes to #43

---
+++ src/SimpleVersion.Core/VersioningEnvironment.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleVersion
+{
+    public class VersioningEnvironment : IEnvironment
+    {
+        public string GetVariable(string name) => System.Environment.GetEnvironmentVariable(name);
+    }
+}
---
+++ test/SimpleVersion.Core.Tests/Pipeline/BuildServers/AzureDevopsProcessFixture.cs
@@ -0,0 +1,75 @@
+﻿using SimpleVersion.Pipeline.BuildServers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using NSubstitute;
+using SimpleVersion.Pipeline;
+using FluentAssertions;
+
+namespace SimpleVersion.Core.Tests.Pipeline.BuildServers
+{
+    public class AzureDevopsProcessFixture
+    {
+        private readonly IEnvironment _env;
+        private AzureDevopsProcess _sut;
+
+        public AzureDevopsProcessFixture()
+        {
+            _env = Substitute.For<IEnvironment>();
+            _sut = new AzureDevopsProcess(_env);
+        }
+
+        [Fact]
+        public void Apply_NotAzure_DoesNotApply()
+        {
+            // Arrange
+            _env.GetVariable("TF_BUILD").Returns("False");
+            var expectedBranchName = "BranchName";
+            var expectedCanonicalBranchName = "CanonicalBranchName";
+            var context = new VersionContext
+            {
+                Result =
+                {
+                    BranchName = expectedBranchName,
+                    CanonicalBranchName = expectedCanonicalBranchName
+                }
+            };
+
+            // Act
+            _sut.Apply(context);
+
+            // Assert
+            context.Result.BranchName.Should().Be(expectedBranchName);
+            context.Result.CanonicalBranchName.Should().Be(expectedCanonicalBranchName);
+        }
+
+        [Theory]
+        [InlineData("refs/heads/master", "master")]
+        [InlineData("refs/heads/release/1.0/master", "release/1.0/master")]
+        [InlineData("refs/pull/10/merge", "pull/10/merge")]
+        public void Apply_IsAzure_DoesApply(string canonical, string name)
+        {
+            // Arrange
+            _env.GetVariable("TF_BUILD").Returns("True");
+            _env.GetVariable("BUILD_SOURCEBRANCH").Returns(canonical);
+            var context = new VersionContext
+            {
+                Result =
+                {
+                    BranchName = "Default Name",
+                    CanonicalBranchName = "Default Canonical Name"
+                }
+            };
+
+            // Act
+            _sut.Apply(context);
+
+            // Assert
+            context.Result.BranchName.Should().Be(name);
+            context.Result.CanonicalBranchName.Should().Be(canonical);
+        }
+    }
+}
