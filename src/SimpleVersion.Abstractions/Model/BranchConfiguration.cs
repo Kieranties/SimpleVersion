a6e6d5a533c8c6e4faa17ff4e003902ec5ce8e0d
Kieranties <Kieran@Kieranties.com>
2019-02-08 15:08:02 +0000

    Base model for overrides

---
+++ src/SimpleVersion.Abstractions/Model/BranchConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleVersion.Model
+{
+    public class BranchConfiguration
+    {
+        public string Match { get; set; } = string.Empty;
+
+        public List<string> Label { get; } = new List<string>();
+
+        public List<string> MetaData { get; } = new List<string>();
+    }
+}
---
+++ src/SimpleVersion.Abstractions/Model/LabelConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVersion.Model
+{
+    public class LabelConfiguration
+    {
+        public List<string> Label { get; } = new List<string>();
+    }
+}
