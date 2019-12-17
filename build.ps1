9a1e05103278f45e6ccc1080cc32afd362d0679b
Kieranties <Kieran@Kieranties.com>
2019-12-17 21:49:42 +0000

    chore: Fix up build for netcore 3.1.00

--- build.ps1
+++ build.ps1
@@ -3,8 +3,8 @@
 #>
 param(
     [ValidateSet('Debug', 'Release')]
-    [String]$Configuration = $env:Configuration,
-    [String]$Version = $env:Version,
+    [String]$Configuration = 'Debug',
+    [String]$Version = '1.0.0-local',
     [String]$RootPath = $PSScriptRoot,
     [String]$ArtifactsPath = (Join-Path $RootPath 'artifacts'),
     [String]$DocsPath = (Join-Path $RootPath 'docs'),
@@ -28,24 +28,17 @@ function exec([string]$cmd) {
 
 $ErrorActionPreference = 'Stop'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
-if(!$Configuration) {
-    $Configuration = 'Debug'
-    $env:Configuration = $Configuration
-}
-if(!$Version) {
-    $Version = '1.0.0-local'
-    $env:Version = $Version
-}
 if($ServeDocs) {
     $BuildDocs = $true
 }
 
+
 # Build/Pack
 Remove-Item $ArtifactsPath -Recurse -Force -ErrorAction Ignore
 if(!$NoBuild) {
-    exec dotnet build
+    exec dotnet build --configuration $Configuration /p:Version=$Version
     $distArtifacts = Join-Path $ArtifactsPath 'dist'
-    exec dotnet pack --no-restore --no-build -o $distArtifacts
+    exec dotnet pack --no-build --no-restore --configuration $Configuration --output $distArtifacts
 
     # Unit Test
     $testArtifacts = Join-Path $ArtifactsPath 'tests'
@@ -53,6 +46,7 @@ if(!$NoBuild) {
         ForEach-Object {
             $testArgs = @(
                 '--no-restore', '--no-build'
+                '--configuration', $Configuration
                 '--logger', 'trx'
                 '-r', $testArtifacts
                 '/p:CollectCoverage=true', "/p:MergeWith=$testArtifacts\coverage.json"
