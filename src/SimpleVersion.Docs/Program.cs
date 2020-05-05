fb1fb808541269ffc5633aebb3b92e8727cccaad
Kieranties <Kieran@Kieranties.com>
2020-05-05 23:14:43 +0100

    Enable trivial schema generation

--- src/SimpleVersion.Docs/Program.cs
+++ src/SimpleVersion.Docs/Program.cs
@@ -1,6 +1,7 @@
 // Licensed under the MIT license. See https://kieranties.mit-license.org/ for full license information.
 
-using System;
+using System.IO;
+using SimpleVersion.Configuration;
 
 namespace SimpleVersion.Docs
 {
@@ -10,34 +11,19 @@ namespace SimpleVersion.Docs
     public sealed class Program
     {
         /// <summary>
-        /// Entry point for SimpleVersion invocation.
+        /// Entry point for Docs invocation.
         /// </summary>
         /// <param name="args">The array of arguments.</param>
-        /// <returns>0 if success, otherwise an error exit code.</returns>
-        public static int Main(string[] args)
+        public static void Main(string[] args)
         {
-            var exitCode = 0;
-            try
+            // TODO: arguments and options
+            var path = System.IO.Directory.GetCurrentDirectory();
+            if (args?.Length > 0)
             {
-                // TODO: arguments and options
-                var path = System.IO.Directory.GetCurrentDirectory();
-                if (args?.Length > 0)
-                {
-                    path = args[0];
-                }
-
-                var calculator = new VersionCalculator(path);
-                calculator.WriteResult(Console.Out);
+                path = args[0];
             }
-#pragma warning disable CA1031 // Do not catch general exception types
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"[Error] {ex.Message}");
-                exitCode = -1;
-            }
-#pragma warning restore CA1031 // Do not catch general exception types
 
-            return exitCode;
+            SchemaGenerator.Generate(typeof(RepositoryConfiguration), Path.Combine(path, "schema.json"));
         }
     }
 }
---
+++ src/SimpleVersion.Docs/SchemaGenerator.cs
@@ -0,0 +1,39 @@
+// Licensed under the MIT license. See https://kieranties.mit-license.org/ for full license information.
+
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using NJsonSchema;
+using NJsonSchema.Generation;
+
+namespace SimpleVersion.Docs
+{
+    /// <summary>
+    /// Generates JsonSchemas.
+    /// </summary>
+    internal static class SchemaGenerator
+    {
+        /// <summary>
+        /// Generates a JsonSchema for the given type.
+        /// </summary>
+        /// <param name="type">The <see cref="Type"/> to generate a schema for.</param>
+        /// <param name="path">The output path of the schema.</param>
+        internal static void Generate(Type type, string path)
+        {
+            var settings = new JsonSchemaGeneratorSettings
+            {
+                SerializerSettings = new JsonSerializerSettings
+                {
+                  ContractResolver = new DefaultContractResolver
+                  {
+                      NamingStrategy = new CamelCaseNamingStrategy()
+                  }
+                }
+            };
+
+            var schema = JsonSchema.FromType(type, settings);
+            File.WriteAllText(path, schema.ToJson());
+        }
+    }
+}
